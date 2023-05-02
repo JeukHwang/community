6bce499a01044c76a8e8ac45fcfc41ad7aefc185
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-05-02 16:17:22 +0900

    feat: connect with mysql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: 'development',
+      entities: [],
+      synchronize: process.env.MODE === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
