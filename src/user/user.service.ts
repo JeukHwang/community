d464ebd087c83e078a4cddd97f04cb08f586ccc7
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-05-10 05:52:50 +0900

    feat: use prisma instead of typeorm

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,33 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { RegisterRequestDto } from 'src/auth/dto/register.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+export type UserProfile = {
+  email: string;
+  surname: string;
+  givenName: string;
+  profilePhoto: string;
+};
+
+export function toProfile(user: User): UserProfile {
+  return {
+    email: user.email,
+    surname: user.surname,
+    givenName: user.givenName,
+    profilePhoto: user.profilePhoto,
+  };
+}
 
 @Injectable()
 export class UserService {
-  async create(
-    surname: string,
-    givenName: string,
-    email: string,
-    password: string,
-  ): Promise<User> {
-    const user = User.create({ surname, givenName, email, password });
-    await user.save();
+  constructor(private readonly prismaService: PrismaService) {}
+  async create(userInfo: RegisterRequestDto): Promise<User | null> {
+    const isEmailUnique =
+      (await this.prismaService.user.count({
+        where: { deletedAt: null, email: userInfo.email },
+      })) == 0;
+    if (!isEmailUnique) {
+      throw new UnauthorizedException('Duplicated email');
+    }
+    const defaultProfilePhoto = 'https://sparcs.org/img/symbol.svg';
+    const user = await this.prismaService.user.create({
+      data: {
+        ...userInfo,
+        profilePhoto: defaultProfilePhoto,
+      },
+    });
     return user;
   }
 
-  findAll(): Promise<User[]> {
-    return User.find();
+  async findAllProfile(): Promise<UserProfile[]> {
+    const users = await this.prismaService.user.findMany({
+      where: { deletedAt: null },
+    });
+    return users.map(toProfile);
   }
 
-  findById(id: string): Promise<User | null> {
-    return User.findOneBy({ id });
+  async findById(id: string): Promise<User | null> {
+    const user = await this.prismaService.user.findFirst({
+      where: { deletedAt: null, id },
+    });
+    return user;
   }
 
-  findByEmail(email: string): Promise<User | null> {
-    return User.findOneBy({ email });
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prismaService.user.findFirst({
+      where: { deletedAt: null, email },
+    });
+    return user;
   }
 
-  async removeById(id: string): Promise<void> {
+  async removeById(id: string) {
+    await this.prismaService.user.deleteMany({
+      where: { deletedAt: null, id },
+    });
+  }
+
+  async setRefreshToken(id: string, refreshToken: string) {
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+    console.log(hashedRefreshToken);
+    await this.prismaService.user.updateMany({
+      where: { deletedAt: null, id },
+      data: { refreshToken: hashedRefreshToken },
+    });
+  }
+
+  async getUserIfRefreshTokenMatches(
+    id: string,
+    refreshToken: string,
+  ): Promise<User | null> {
     const user = await this.findById(id);
-    await User.softRemove(user);
+    const isRefreshTokenMatching = await bcrypt.compare(
+      refreshToken,
+      user.refreshToken,
+    );
+    return isRefreshTokenMatching ? user : null;
+  }
+
+  removeRefreshToken(id: string) {
+    this.prismaService.user.updateMany({
+      where: { deletedAt: null, id },
+      data: { refreshToken: null },
+    });
   }
 }
