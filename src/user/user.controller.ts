eb5b70ee26a462ca8910607f032180dec835427d
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-05-11 02:59:52 +0900

    refactor: use CurrentUser decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { Public } from 'src/auth/decorator/skip-auth.decorator';
+import { CurrentUser } from './decorator/current.decorator';
 import { UserProfile, UserService } from './user.service';
 
 @Controller('user')
@@ -14,8 +15,7 @@ export class UserController {
   }
 
   @Get('current')
-  async getCurrentUser(@Req() req): Promise<User> {
-    const user = req.user;
+  async getCurrentUser(@CurrentUser() user): Promise<User> {
     return user;
   }
 }
