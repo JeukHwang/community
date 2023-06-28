095e0e38bc8c075d3cbd35d6d3602a9b084161be
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-06-28 23:46:39 +0900

    feat: add test apis for s3 and class-validator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,26 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  StreamableFile,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Max, MaxLength, Min, MinLength } from 'class-validator';
 import { AppService } from './app.service';
 import { Public } from './auth/decorator/skip-auth.decorator';
+import { download, upload } from './s3';
+
+class TestTypeCheckDto {
+  @MinLength(5)
+  @MaxLength(10)
+  string: string;
+  @Min(-10)
+  @Max(10)
+  number: number;
+}
 
 @Controller()
 export class AppController {
@@ -21,5 +41,28 @@ export class AppController {
   @Get('/test/public-ping')
   getPrivate(@Body() body: JSON): string {
     return this.appService.getPingPublicly(body);
+  }
+
+  @Public()
+  @Post('/test/public-upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    // console.log(file);
+    const result = await upload(file);
+    // console.log(result);
+    return { key: result.Key };
+  }
+
+  @Public()
+  @Post('/test/public-download/')
+  async downloadFile(@Body() body) {
+    const result = await download(body.key);
+    return new StreamableFile(result);
+  }
+
+  @Public()
+  @Post('/test/public-typecheck')
+  async typeCheck(@Body() body: TestTypeCheckDto) {
+    return body;
   }
 }
